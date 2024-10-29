16bf548402a6b04eb1a219d8498b405d653fa460
BeautySea <benjaminlin53@outlook.com>
2024-10-30 02:56:33 +0900

    first commit

---
+++ src/functions/httpTrigger1.ts
@@ -0,0 +1,15 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function httpTrigger1(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+    const body = await request.json()
+    const name = request.query.get('name') || body["name"] || 'world';
+
+    return { body: `Hello, ${name}!` };
+};
+
+app.http('httpTrigger1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: httpTrigger1
+});
